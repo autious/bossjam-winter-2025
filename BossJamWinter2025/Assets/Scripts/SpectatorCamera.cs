e34b0d50c785c58438fbc33a882e370aef82e6ac
Kroporo <michele.butkus@octacon.se>
2025-11-22 23:06:25 +0100

    Make audio listener on spectator camera activate when needed

--- BossJamWinter2025/Assets/Scripts/SpectatorCamera.cs
+++ BossJamWinter2025/Assets/Scripts/SpectatorCamera.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpectatorCamera : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class SpectatorCamera : MonoBehaviour {
+    private AudioListener audioListener;
+
+    protected void Awake() {
+        audioListener = GetComponent<AudioListener>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    protected void Update() {
+        audioListener.enabled = Camera.main == null || Camera.main.gameObject == gameObject;
     }
 }
