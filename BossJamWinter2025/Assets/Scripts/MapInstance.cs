5a48f3a55f7ece0fbf90926c0bbc783984aa1d1c
Kroporo <michele.butkus@octacon.se>
2025-11-22 12:49:59 +0100

    Make spawnpoint selection a little smarter

--- BossJamWinter2025/Assets/Scripts/MapInstance.cs
+++ BossJamWinter2025/Assets/Scripts/MapInstance.cs
@@ -45,7 +45,35 @@ public class MapInstance : NetworkBehaviour {
     }
 
     private void SpawnOwnPlayer() {
-        var spawnPoint = GameObject.FindObjectsByType<SpawnPointPlayer>(FindObjectsSortMode.None).GetRandom(); // TODO Find a spawn point that doesn't have other people around it
+        var players = FindObjectsByType<SpawnPointPlayer>(FindObjectsSortMode.None);
+        var spawnPoints = FindObjectsByType<SpawnPointPlayer>(FindObjectsSortMode.None); // Imagine caching any of this
+
+        // Get some spawn points that are far enough from other players
+        const float MIN_DISTANCE = 10.0f;
+        var validPoints = new List<SpawnPointPlayer>();
+        foreach (var potentialSpawnPoint in spawnPoints) {
+            bool valid = false;
+            foreach (var player in players) {
+                if (Vector3.Distance(potentialSpawnPoint.transform.position, player.transform.position) < MIN_DISTANCE) {
+                    valid = false;
+                }
+            }
+
+            // TODO Add a raycast to make sure we don't spawn visible to other players
+
+            if (valid) {
+                validPoints.Add(potentialSpawnPoint);
+            }
+        }
+
+        // Select the spawn point to actually use
+        SpawnPointPlayer spawnPoint = spawnPoints.GetRandom();
+        if (validPoints.Count > 0) {
+            spawnPoint = validPoints.GetRandom();
+        } else {
+            Debug.LogWarning("Unable to find a suitable spawn point, choosing a random one");
+        }
+
         Runner.Spawn(playerPrefab, spawnPoint.transform.position + Vector3.up, spawnPoint.transform.rotation, Runner.LocalPlayer);
     }
 
